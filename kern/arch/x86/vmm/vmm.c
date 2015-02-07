1873475b8daa55efedb5ca5359ccec24eb41f31c
Ron Minnich <rminnich@gmail.com>
2015-02-06 16:50:09 -0800

    VMMCP: FIRST COMMIT
    Couple things here.
    
    We establish the principle that vmm is always in there. It's not optional.
    
    This ensures that everything always builds.
    
    We don't conditionally compile in anything. If we want variant behavior,
    we dynamically key on cpuinfo. It's been learned the hard way that this is the
    way to go.
    
    Signed-off-by: Ron Minnich <rminnich@gmail.com>

---
+++ kern/arch/x86/vmm/vmm.c
@@ -0,0 +1,17 @@
+/* Copyright 2015 Google Inc.
+ * 
+ * See LICENSE for details.
+ */
+
+/* We're not going to falll into the trap of only compiling support
+ * for AMD OR Intel for an image. It all gets compiled in, and which
+ * one you use depends on on cpuinfo, not a compile-time
+ * switch. That's proven to be the best strategy.  Conditionally
+ * compiling in support is the path to hell.
+ */
+
+#include <pmap.h>
+#include "intel/vmx_cpufunc.h"
+
+/* this will be the init function for vmm. For now, it just ensures we
+   don't break things. */
