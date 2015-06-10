5288dc44268b037f8ffabfb4846c02f26387b52f
Ronald G. Minnich <rminnich@gmail.com>
2015-06-10 12:12:03 -0700

    Run a kernel.

--- tests/vmrunkernel.c
+++ tests/vmrunkernel.c
@@ -11,8 +11,35 @@
 #include <string.h>
 #include <ros/syscall.h>
 #include <sys/mman.h>
+#define PML4_SHIFT		39
+#define PML3_SHIFT		30
+#define PML2_SHIFT		21
+#define PML1_SHIFT		12
+#define BITS_PER_PML	9
 
-int *mmap_blob;
+/* PTE reach is the amount of VM an entry can map, either as a jumbo or as
+ * further page tables.  I'd like to write these as shifts, but I can't please
+ * both the compiler and the assembler. */
+#define PML4_PTE_REACH	(0x0000008000000000)	/* No jumbos available */
+#define PML3_PTE_REACH	(0x0000000040000000)	/* 1 GB jumbos available */
+#define PML2_PTE_REACH	(0x0000000000200000)	/* 2 MB jumbos available */
+#define PML1_PTE_REACH	(0x0000000000001000)	/* aka, PGSIZE */
+
+/* Reach is the amount of VM a table can map, counting all of its entries.
+ * Note that a PML(n)_PTE is a PML(n-1) table. */
+#define PML3_REACH		(PML4_PTE_REACH)
+#define PML2_REACH		(PML3_PTE_REACH)
+#define PML1_REACH		(PML2_PTE_REACH)
+
+/* PMLx(la, shift) gives the 9 bits specifying the la's entry in the PML
+ * corresponding to shift.  PMLn(la) gives the 9 bits for PML4, etc. */
+#define PMLx(la, shift)	(((uintptr_t)(la) >> (shift)) & 0x1ff)
+#define PML4(la) 		PMLx(la, PML4_SHIFT)
+#define PML3(la) 		PMLx(la, PML3_SHIFT)
+#define PML2(la) 		PMLx(la, PML2_SHIFT)
+#define PML1(la) 		PMLx(la, PML1_SHIFT)
+
+static void *mmap_blob;
 unsigned long long stack[1024];
 volatile int tr, rr, done;
 volatile int state;
@@ -48,18 +75,25 @@ int nr_threads = 2;
 
 int main(int argc, char **argv)
 {
+	int i;
 	int nr_gpcs = 1;
 	uint64_t entry;
 	int fd = open("#c/sysctl", O_RDWR), ret;
 	int kfd = -1;
+	bool smallkernel = false;
 	void * x;
 	static char cmd[512];
 	if (fd < 0) {
 		perror("#c/sysctl");
 		exit(1);
 	}
-	if argc != 3 {
-		fprintf(stderr, "Usage: %s [vm image] [entry point]\n", argv[0]);
+	argc--,argv++;
+	if (! strcmp(argv[0], "-s")) {
+		smallkernel = true;
+		argc--,argv++;
+	}
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s [-s] vmimage entrypoint\n", argv[0]);
 		exit(1);
 	}
 	entry = strtoull(argv[2], 0, 0);
@@ -134,13 +168,29 @@ int main(int argc, char **argv)
 	p512 = mmap_blob;
 	p1 = &p512[512];
 	p2m = &p512[1024];
-	p512[0] = (unsigned long long)p1 | 7;
-	p1[0] = /*0x87; */(unsigned long long)p2m | 7;
-	p2m[0] = 0x87;
-	p2m[1] = 0x200000 | 0x87;
-	p2m[2] = 0x400000 | 0x87;
-	p2m[3] = 0x600000 | 0x87;
-
+	// Map the top 2G of kernel address space. It's ok to map memory
+	// we have no access to. Further, since the PML2 is the same in all cases,
+	// two PML3s can point to it.
+	// Just assume we get the maximum, and build your kernels accordingly.
+#define KERNBASElow 0xffffffff80000000
+#define KERNBASEhigh 0xffffffffc0000000
+#define KERNBASEsmall 0xfffffffff0000000
+#define _2MiB 0x200000
+	if (smallkernel) {
+		p512[PML4(KERNBASEsmall)] = (unsigned long long)p1 | 7;
+		p1[PML3(KERNBASEsmall)] = /*0x87; */(unsigned long long)p2m | 7;
+		// to make it easy, we add KERNBASE to entry for you. Less typing.
+		entry += KERNBASEsmall;
+	} else {
+		p512[PML4(KERNBASEhigh)] = (unsigned long long)p1 | 7;
+		p1[PML3(KERNBASEhigh)] = /*0x87; */(unsigned long long)p2m | 7;
+		p1[PML3(KERNBASElow)] = /*0x87; */(unsigned long long)p2m | 7;
+		// to make it easy, we add KERNBASE to entry for you. Less typing.
+		entry += KERNBASElow;
+	}
+	for(i = 0; i < 512; i++)
+		p2m[PML2(KERNBASElow) + i * _2MiB] = 0x87 | _2MiB;
+			
 	printf("p512 %p p512[0] is 0x%lx p1 %p p1[0] is 0x%x\n", p512, p512[0], p1, p1[0]);
 	sprintf(cmd, "V 0x%x 0x%x 0x%x", entry, (unsigned long long) &stack[1024], (unsigned long long) p512);
 	printf("Writing command :%s:\n", cmd);
